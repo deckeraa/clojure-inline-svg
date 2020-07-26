abbd20eefe614e513ad6cd92d8409f4f5fac1658
Aaron Decker <decker49@gmail.com>
2020-07-26 16:13:05 -0500

    Finish trash -> zoom-out

---
+++ project.clj
@@ -0,0 +1,47 @@
+(defproject clojure-inline-svgs "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [javax.xml.bind/jaxb-api "2.2.11"]
+                 [reagent "0.7.0"]]
+
+  :min-lein-version "2.5.3"
+
+  :source-paths ["src/clj"]
+
+  :plugins [[lein-cljsbuild "1.1.4"]]
+
+  :clean-targets ^{:protect false} ["resources/public/js"
+                                    "target"]
+
+  :figwheel {:css-dirs ["resources/public/css"]}
+
+  :profiles
+  {:dev
+   {:dependencies []
+
+    :plugins      [[lein-figwheel "0.5.15"]]
+    }}
+
+  :cljsbuild
+  {:builds
+   [{:id           "dev"
+     :source-paths ["src/cljs"]
+     :figwheel     {:on-jsload "clojure-inline-svgs.core/reload"}
+     :compiler     {:main                 clojure-inline-svgs.core
+                    :optimizations        :none
+                    :output-to            "resources/public/js/app.js"
+                    :output-dir           "resources/public/js/dev"
+                    :asset-path           "js/dev"
+                    :source-map-timestamp true}}
+
+    {:id           "min"
+     :source-paths ["src/cljs"]
+     :compiler     {:main            clojure-inline-svgs.core
+                    :optimizations   :advanced
+                    :output-to       "resources/public/js/app.js"
+                    :output-dir      "resources/public/js/min"
+                    :elide-asserts   true
+                    :closure-defines {goog.DEBUG false}
+                    :pretty-print    false}}
+
+    ]})
